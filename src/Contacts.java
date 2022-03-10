46d4065e89a44b949362ce9ca8c4deb9892c69e9
Justin Conger <jhconger@GMAIL.COM>
2022-03-10 14:44:15 -0600

    building framework

--- src/Contacts.java
+++ src/Contacts.java
@@ -1,2 +1,31 @@
 public class Contacts {
+    private String name;
+    private String number;
+    public Contacts(){
+
+    }
+    public Contacts(String name, String number){
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String returnContacts(){
+        return this.name + " " + this.number;
+    }
 }
--- src/ContactsApp.java
+++ src/ContactsApp.java
@@ -1,2 +1,28 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Scanner;
+
 public class ContactsApp {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. View contacts.");
+        System.out.println("2. Add a new contact.");
+        System.out.println("3. Search a contact by name.");
+        System.out.println("4. Delete an existing contact.");
+        System.out.println("5. Exit.");
+        System.out.println("Enter an option (1, 2, 3, 4 or 5):");
+
+    }
+
+
+    public static void printData(Path filePath) throws IOException {
+        System.out.println();
+        List<String> fileContents = Files.readAllLines(filePath); // we need List as .readAllLines returns List datatype
+        for (int i = 0; i < fileContents.size(); i++) {
+            System.out.printf("%d: %s\n", i + 1, fileContents.get(i));
+        }
+    }
 }
