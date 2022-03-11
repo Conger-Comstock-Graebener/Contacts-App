8860a3567e76d0b5ad1d8a954eee8489208e8bc5
Justin Conger <jhconger@GMAIL.COM>
2022-03-10 18:38:22 -0600

    added formatting

--- src/ContactsApp.java
+++ src/ContactsApp.java
@@ -8,32 +8,56 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-    public class ContactsApp
+import static jdk.internal.org.jline.utils.Colors.s;
+
+public class ContactsApp
     {
         static Path pathToContacts = Paths.get("src/data", "contacts.txt");
 
         public static Scanner sc = new Scanner(System.in);
+        public static Contacts[] contactsList;
 
-        public static void printAllContacts(Path filePath) throws IOException
+        public static void printAllContacts(Path pathToContacts) throws IOException
         {
             System.out.println();
-            List<String> fileContents = Files.readAllLines(filePath); // we need List as .readAllLines returns List datatype
+            List<Contacts> contactsList = createContactsObject();
+            List<String> fileContents = Files.readAllLines(pathToContacts); // we need List as .readAllLines returns List datatype
             for (int i = 0; i < fileContents.size(); i++)
             {
-                System.out.printf("%d: %s\n", i + 1, fileContents.get(i));
+                System.out.println("");
+                System.out.printf("%-19s%s%s\n","Name", "| ", "Phone number");
+                System.out.println("--------------------------------");
+                for (Contacts contact :contactsList){
+//                System.out.println(contact.returnContacts());
+                    System.out.printf( "%-19s%s%s\n", contact.getName(),"| ", contact.getNumber()  );
+                }
             }
         }
+        public static List<Contacts> createContactsObject(){
+            List<Contacts> contacts = new ArrayList<>();
+            try {
+                Path pathToOurFile = Paths.get("src/data", "contacts.txt");
+                List<String> contactsList = Files.readAllLines(pathToOurFile);
 
+                for (String contact : contactsList){
+                    Contacts contact1 = new Contacts(contact.substring(0, contact.lastIndexOf(" ")),
+                            contact.substring(contact.lastIndexOf(" ") + 1));
+                    contacts.add(contact1);
+                }
+            } catch (IOException ioe){
+                ioe.printStackTrace();
+            }
+            return contacts;
+        }
         public static void appStart() throws IOException {
+                    System.out.println("1. View contacts.");
+                System.out.println("2. Add a new contact.");
+                System.out.println("3. Search a contact by name.");
+                System.out.println("4. Delete an existing contact.");
+                System.out.println("5. Exit.");
+                System.out.println("Enter an option (1, 2, 3, 4 or 5):");
 
-            System.out.println("1. View contacts.");
-            System.out.println("2. Add a new contact.");
-            System.out.println("3. Search a contact by name.");
-            System.out.println("4. Delete an existing contact.");
-            System.out.println("5. Exit.");
-            System.out.println("Enter an option (1, 2, 3, 4 or 5):");
-
-            String userInput = sc.nextLine();
+                String userInput = sc.nextLine();
             switch (userInput)
             {
                 case "1":
@@ -50,7 +74,7 @@ import java.util.Scanner;
                     break;
 
                 case "3":
-//                    todo seatch by name
+//                    todo search by name
                     System.out.println();
                     break;
                 case "4":
@@ -77,7 +101,7 @@ import java.util.Scanner;
             List<String> newContact = Arrays.asList(person);
 //            Contacts contact =  new Contacts(userName, userNumber);
 
-            Files.write(pathToContacts, newContact, StandardOpenOption.APPEND);
+//            Files.write(pathToContacts, newContact, StandardOpenOption.APPEND);
 
         }
 
